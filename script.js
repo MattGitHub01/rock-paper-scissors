3f511df138d1128dc61440c5f4a4a14466adba39
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-03-13 18:43:30 -0600

    Add script.js and create pseudocode for three main functions

---
+++ script.js
@@ -0,0 +1,21 @@
+function getComputerChoice() {
+    // automatically generate computer choice numerically(?)
+    // associate that to Rock, Paper or Scissors
+    // return random choice
+}
+
+function playRound(userInput) {
+    // check if input is a string / either rock, paper or scissors
+        //if not then return "Wrong input! Please choose rock, paper or scissors!"
+    // convert user input into lower case
+    // run getComputerChoice()
+    // check userInput against computerChoice()
+    // if player wins return "You Win! [userInput] beats [computerChoice()]"
+    // if computer wins return "You Lose! [computerChoice() beats [userInput]"
+
+}
+
+function playGame() {
+    //don't use loops because prompt
+    //play five instances of playRound
+}
