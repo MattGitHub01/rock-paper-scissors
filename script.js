6483e9b45b964924e9b700dcf835da0d6948db43
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-03-13 18:54:15 -0600

    Start playRound function, change comp choices lower case

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 function getComputerChoice() {
     let compChoice = "";
-    // automatically generate computer choice numerically(?)
     let compNum = Math.random() * (3 - 0) + 0;
-    // associate that to Rock, Paper or Scissors
     if (compNum === 0 ) {
-        compChoice === "Rock";
+        compChoice === "rock";
         return compChoice
     } else if (compNum === 1) {
-        compChoice === "Paper";
+        compChoice === "paper";
         return compChoice
     } else if (compNum === 2) {
-        compChoice === "Scissors";
+        compChoice === "scissors";
         return compChoice
     } else {
         console.log("Error, please reset program");
         return compChoice
     }
-    // return random choice
 }
 
 function playRound(userInput) {
-    // check if input is a string / either rock, paper or scissors
-        //if not then return "Wrong input! Please choose rock, paper or scissors!"
+    userInput = userInput.toLowerCase();
     // convert user input into lower case
+    let computerInput = getComputerChoice();
     // run getComputerChoice()
+
     // check userInput against computerChoice()
+        // check if input is a string / either rock, paper or scissors
+            //if not then return "Wrong input! Please choose rock, paper or scissors!"
     // if player wins return "You Win! [userInput] beats [computerChoice()]"
     // if computer wins return "You Lose! [computerChoice() beats [userInput]"
 
